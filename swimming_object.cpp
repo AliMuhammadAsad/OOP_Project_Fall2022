420cec3369ac6659c57e3a55874c093d1b3bd047
Ali Muhammad Asad <aa07190@st.habib.edu.pk>
2022-11-13 13:23:51 +0500

    swimming_object parent class

---
+++ swimming_object.cpp
@@ -0,0 +1,15 @@
+#include "swimming_object.hpp"
+
+Swimming_Object::Swimming_Object(SDL_Rect src, SDL_Rect mover){
+    srcRect = src; moverRect = mover; //setting srcRect and moverRect values
+}
+
+void Swimming_Object::fly(){};
+
+void Swimming_Object::draw(){ //draw function to be inherited by all child classes
+    SDL_RenderCopy(Drawing::gRenderer, Drawing::assets, &srcRect, &moverRect);
+}
+
+SDL_Rect Swimming_Object::getMov(){
+    return moverRect; //getting moverRect value
+}
---
+++ swimming_object.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <SDL.h>
+#include "drawing.hpp"
+
+class Swimming_Object{
+    protected:
+    SDL_Rect srcRect, moverRect;
+
+    public:
+    Swimming_Object(SDL_Rect, SDL_Rect); //constructor
+    void draw(); //draw that will be inherited by all child classes as is
+    virtual void swim(); //virtual swim function that will be overrided by child classes - achieves polymorphism
+    SDL_Rect getMov(); //getting Mover rect position
+};
