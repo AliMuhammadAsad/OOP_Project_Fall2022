17fb4f54536762a3082e1eb2dfcbb30d10921e6c
Ali Muhammad Asad <aa07190@st.habib.edu.pk>
2022-11-30 14:58:17 +0500

    Somethings

--- score.cpp
+++ score.cpp
@@ -2,10 +2,10 @@
 
 Score::Score(int init_score) : score(init_score){};
 
-void Score::AddScore(){
-    score += 10; //whatever amount we wanna add to the score on eating a fish - 10 is a dummy value for now
-}
-
 int Score::getScore(){
     return score; //getting the score to be displayed etc
+}
+
+void Score::operator++(){
+    score = score + 10; //whatever amount we wanna add to the score on eating a fish - 10 is a dummy value for now 
 }
